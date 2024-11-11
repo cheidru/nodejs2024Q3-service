cf26c53a8f2716d49a5c1725d2bea59bd50a156e
Igor Checheurov <chei@yandex.ru>
2024-11-11 16:53:50 +0300

    feat: add validation to CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,9 @@
 import { IsAlphanumeric, IsInt, IsUUID, Length } from 'class-validator';
 
 export class CreateUserDto {
-  // @IsUUID()
-  // id: string;
   @Length(3)
   @IsAlphanumeric()
   login: string;
   @Length(3)
   password: string;
-  // @IsInt()
-  // version: number; // integer number, increments on update
-  // @IsInt()
-  // createdAt: number; // timestamp of creation
-  // @IsInt()
-  // updatedAt: number; // timestamp of last update
 }
